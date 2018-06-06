108c96b9389dd9850299d0df7ee661d0d7644901
Mateusz Kuźmik <mateusz.kuzmik1@gmail.com>
2018-06-06 23:59:48 +0200

    Category and element displayed.

--- src/app/app.js
+++ src/app/app.js
@@ -2,24 +2,45 @@ import angular from 'angular';
 
 import '../style/app.css';
 
-let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
-};
-
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
-
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+const module = angular.module(MODULE_NAME, []);
+
+module.component('home', {
+  template: require('./home.html'),
+  controller: function () {}
+});
+
+module.component('categories', {
+  template: require('./categories.html'),
+  controller: function ($http) {
+    let that = this;
+
+    $http.get('http://localhost:8080/forests').then(response => {
+      that.categories = response.data;
+    });
+
+    that.show = (category) => {
+      if (that.chosenCategory !== category)
+        that.chosenCategory = category;
+      else
+        that.chosenCategory = undefined;
+    };
+  }
+});
+
+module.component('elements', {
+  template: require('./elements.html'),
+  bindings: {
+    category: '<'
+  },
+  controller: function ($scope) {
+    let that = this;
+
+    that.$onChanges = function (changes) {
+      that.elves = changes.category.currentValue.elves;
+    };
+  }
+});
 
 export default MODULE_NAME;
--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = function makeWebpackConfig() {
 
     // Output path from the view of the page
     // Uses webpack-dev-server in development
-    publicPath: isProd ? '/' : 'http://0.0.0.0:8080/',
+    publicPath: isProd ? '/' : 'http://localhost:8000/',
 
     // Filename for entry points
     // Only adds hash in build mode
@@ -218,7 +218,8 @@ module.exports = function makeWebpackConfig() {
   config.devServer = {
     contentBase: './src/public',
     stats: 'minimal',
-    host: '0.0.0.0'
+    host: 'localhost',
+    port: '8000'
   };
 
   return config;
